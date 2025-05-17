bebd7d9c644bfe3ec49afd72de609f84477de8ba
Saanya <saanya03@umd.edu>
2025-05-17 17:03:33 -0400

    Test Deployment

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -12,7 +12,7 @@ const skillCategories = [
   },
   {
     title: "Testing & Automation",
-    skills: ["Selenium", "JUnit", "TestNG", "POM", "Cucumber", "Cypress", "Postman", "Hello"]
+    skills: ["Selenium", "JUnit", "TestNG", "POM", "Cucumber", "Cypress", "Postman"]
   },
   {
     title: "Cloud & Security",
